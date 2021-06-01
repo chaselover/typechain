3c90f7c74c6bfa29c1a73036905b91ba55d7cb05
Han Seung Joo <gkstmdwn234@gmail.com>
2021-06-01 23:32:53 +0900

    make blockchain

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
 import * as CryptoJS from "crypto-js";
 
 class Block {
-  public index: number;
-  public hash: string;
-  public previousHash: string;
-  public data: string;
-  public timestamp: number;
-
   // 메소드가 Block클래스 안에 있어야함. BLock안에서 언제나 호출할 수 있는 메소드.
   //하지만 우리는 블록 생성 안해도 사용가능한 method를 만들고싶음.
   //static 메소드 쓰면 클래스 밖에서도 쓸수있음.
+  //index.js가보면 class밖에서 선언되어있음.
   static calculateBlockHash = (
     index: number,
     previousHash: string,
@@ -18,6 +13,22 @@ class Block {
   ): string => {
     return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
   };
+
+  static validateStructure = (aBlock: Block): boolean => {
+    return (
+      typeof aBlock.index === "number" &&
+      typeof aBlock.hash === "string" &&
+      typeof aBlock.previousHash === "string" &&
+      typeof aBlock.timestamp === "number" &&
+      typeof aBlock.data === "string"
+    );
+  };
+
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
 
   constructor(
     index: number,
@@ -38,16 +49,76 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "238793232", "", "Hello", 123456);
 
+//이런식으로 설계해 배열이 아닌게  push 되지 않도록 막음.
+//아름다운 typescript
 let blockchain: Block[] = [genesisBlock];
 
 //blockchain에 stuff를 push하수없음을 알게됨(type script의 장점.)
 //blockchain.push("stuff");
 
-console.log(blockchain);
+//console.log(blockchain);
 const getBlockchain = (): Block[] => blockchain;
 
-const getLatesBlock = (): Block => blockchain[blockchain.length - 1];
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimeStamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimeStamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimeStamp
+  );
+  addBlock(newBlock);
+  return newBlock;
+};
+
+const getHashforBlock = (aBlock: Block): string =>
+  Block.calculateBlockHash(
+    aBlock.index,
+    aBlock.previousHash,
+    aBlock.timestamp,
+    aBlock.data
+  );
+
+//console.log(createNewBlock("hello"), createNewBlock("bye bye"));
+//블록체인의 기반은 블록들이 자신의 전 블록으로 링크되어 있다는것!!!
+//타당성 검사를 통해 후보 블록과 이전블록을 검사.
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
+  }
+};
+
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+
+console.log(blockchain);
 
 export {};
